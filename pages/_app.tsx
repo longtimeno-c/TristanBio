225c4e05abf076e50ed7d08281c27a8e4ebca614
Tristan Hill <tphwoodlands@gmail.com>
2025-04-06 10:46:38 +0100

    updated favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,7 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {/* Prevent hydration mismatch for dark mode */}
+      <Head>
+        <link rel="icon" href="../components/favi2.png" type="image/png" />
+        <meta name="theme-color" content="#0f172a" />
+      </Head>
       {mounted && <Component {...pageProps} />}
     </>
   )
